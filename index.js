d7ed0bb8b83e91d38d32ebb15ea48996d6e3bd16
Kam <Kamwescott@gmail.com>
2022-07-07 16:03:49 -0700

    prefinal stable

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ let name;
 let typeArr=[];
 let type;
 
+let preEvoArr=[]
+let preEvo;
+
+let evoArr=[];
+let evo;
+
+
+
 
 
 function generateNewPokemon(){
@@ -21,31 +29,73 @@ for(let result of d.data){
       arr.push(result.images.small)
       nameArr.push(result.name)
       typeArr.push(result.types)
-      console.log(result)
+      preEvoArr.push(result.evolvesFrom)
+      evoArr.push(result.evolvesTo)
+      //console.log(result)
+      
    }
 }
+console.log(evoArr)
 // assigns a random index in arr to img
 $("body").append('<div class="container"></div>')
 img = arr[Math.floor(Math.random()*31)]
 name= nameArr[arr.indexOf(img)]
 type=typeArr[arr.indexOf(img)]
+
+if(preEvoArr[arr.indexOf(img)] === undefined){ // if preEvo is undefined = N/A otherwise correct
+   preEvo="N/A"
+} else {
+   preEvo=preEvoArr[arr.indexOf(img)]
+}
+if(evoArr[arr.indexOf(img)] === undefined){ // --> *** 'arr' is the img array ***
+   evo = "N/A"
+} else {
+   evo = evoArr[arr.indexOf(img)]
+}
 $('#card1').append(`<img id="img1" src="${img}"></img>`)
 $('.name1-fill').append(`   ${name}`)
 $('.type1-fill').append(`   ${type}`)
+$('.preEvo1-fill').append(` ${preEvo}`)
+$('.evo1-fill').append(` ${evo}`)
+
 $('#img1').addClass("card-img-top")
 img = arr[Math.floor(Math.random()*31)]
 name= nameArr[arr.indexOf(img)]
 type=typeArr[arr.indexOf(img)]
+if(preEvoArr[arr.indexOf(img)] === undefined){ 
+   preEvo="N/A"
+} else {
+   preEvo=preEvoArr[arr.indexOf(img)]
+}
+if(evoArr[arr.indexOf(img)] === undefined){ // --> *** 'arr' is the img array ***
+   evo = "N/A"
+} else {
+   evo = evoArr[arr.indexOf(img)]
+}
 $('#card2').append(`<img id="img2" src="${img}"></img>`)
 $('.name2-fill').append(`   ${name}`)
 $('.type2-fill').append(`   ${type}`)
+$('.preEvo2-fill').append(` ${preEvo}`)
+$('.evo2-fill').append(` ${evo}`)
 $('#img2').addClass("card-img-top")
 img = arr[Math.floor(Math.random()*31)]
 name= nameArr[arr.indexOf(img)]
 type=typeArr[arr.indexOf(img)]
+if(preEvoArr[arr.indexOf(img)] === undefined){ 
+   preEvo="N/A"
+} else {
+   preEvo=preEvoArr[arr.indexOf(img)]
+}
+if(evoArr[arr.indexOf(img)] === undefined){ // --> *** 'arr' is the img array ***
+   evo = "N/A"
+} else {
+   evo = evoArr[arr.indexOf(img)]
+}
 $('#card3').append(`<img id="img3" src="${img}"></img>`)
 $('.name3-fill').append(`   ${name}`)
 $('.type3-fill').append(`   ${type}`)
+$('.preEvo3-fill').append(` ${preEvo}`)
+$('.evo3-fill').append(` ${evo}`)
 $('#img3').addClass("card-img-top")
 //console.log(img)
 
@@ -61,23 +111,61 @@ $(".fill").text("")
 img = arr[Math.floor(Math.random()*31)]
 name= nameArr[arr.indexOf(img)]
 type=typeArr[arr.indexOf(img)]
+
+if(preEvoArr[arr.indexOf(img)] === undefined){ // if preEvo is undefined = N/A otherwise correct
+   preEvo="N/A"
+} else {
+   preEvo=preEvoArr[arr.indexOf(img)]
+}
+if(evoArr[arr.indexOf(img)] === undefined){ // --> *** 'arr' is the img array ***
+   evo = "N/A"
+} else {
+   evo = evoArr[arr.indexOf(img)]
+}
 $('#card1').append(`<img id="img1" src="${img}"></img>`)
 $('.name1-fill').append(`   ${name}`)
 $('.type1-fill').append(`   ${type}`)
+$('.preEvo1-fill').append(` ${preEvo}`)
+$('.evo1-fill').append(` ${evo}`)
+
 $('#img1').addClass("card-img-top")
 img = arr[Math.floor(Math.random()*31)]
 name= nameArr[arr.indexOf(img)]
 type=typeArr[arr.indexOf(img)]
+if(preEvoArr[arr.indexOf(img)] === undefined){ 
+   preEvo="N/A"
+} else {
+   preEvo=preEvoArr[arr.indexOf(img)]
+}
+if(evoArr[arr.indexOf(img)] === undefined){ // --> *** 'arr' is the img array ***
+   evo = "N/A"
+} else {
+   evo = evoArr[arr.indexOf(img)]
+}
 $('#card2').append(`<img id="img2" src="${img}"></img>`)
 $('.name2-fill').append(`   ${name}`)
 $('.type2-fill').append(`   ${type}`)
+$('.preEvo2-fill').append(` ${preEvo}`)
+$('.evo2-fill').append(` ${evo}`)
 $('#img2').addClass("card-img-top")
 img = arr[Math.floor(Math.random()*31)]
 name= nameArr[arr.indexOf(img)]
 type=typeArr[arr.indexOf(img)]
+if(preEvoArr[arr.indexOf(img)] === undefined){ 
+   preEvo="N/A"
+} else {
+   preEvo=preEvoArr[arr.indexOf(img)]
+}
+if(evoArr[arr.indexOf(img)] === undefined){ // --> *** 'arr' is the img array ***
+   evo = "N/A"
+} else {
+   evo = evoArr[arr.indexOf(img)]
+}
 $('#card3').append(`<img id="img3" src="${img}"></img>`)
 $('.name3-fill').append(`   ${name}`)
 $('.type3-fill').append(`   ${type}`)
+$('.preEvo3-fill').append(` ${preEvo}`)
+$('.evo3-fill').append(` ${evo}`)
 $('#img3').addClass("card-img-top")
   })
 
